7d63ebe65f625468747d9612f58df4f9d9b3eafe
Antonio Murcia <amurciaa61@gmail.com>
2020-12-08 08:17:54 +0100

    Agregar archivos de proyecto.

---
+++ Superheroes_Trigger/Covertidor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Superheroes_Trigger
+{
+    class Covertidor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                if ((bool)value)
+                    return true;
+                else
+                    return false;
+            }
+            else
+                return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Superheroes_Trigger/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+
+namespace Superheroes_Trigger
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Superheroe> heroes;
+        Superheroe heroe = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            heroes = Superheroe.GetSamples();
+            BindingDelObjeto(1);
+            ActualizaIndice(1);
+        }
+
+        private void FlechaAdelanteImage_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            int indiceActual = ObtenerIndiceActual();
+            int indice = (indiceActual == heroes.Count) ? 1 : indiceActual + 1;
+            BindingDelObjeto(indice);
+            ActualizaIndice(indice);
+        }
+        private void FlechaAtrasImage_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            int indiceActual = ObtenerIndiceActual();
+            int indice = (indiceActual == 1) ? heroes.Count : indiceActual - 1;
+            BindingDelObjeto(indice);
+            ActualizaIndice(indice);
+        }
+        public void BindingDelObjeto(int indice)
+        {
+            indice--;
+            pestaña1DockPanel.DataContext = heroes[indice];
+            heroe = new Superheroe();
+            pestaña2Grid.DataContext = heroe;
+            heroe.Heroe = true;
+        }
+        public void ActualizaIndice(int indice)
+        {
+            numeroImagenTextBlock.Text = indice.ToString() + "/" + heroes.Count.ToString();
+        }
+        public int ObtenerIndiceActual()
+        {
+            string[] valores = numeroImagenTextBlock.Text.Split('/');
+            return Int32.Parse(valores[0]);
+        }
+        private void AceptarButton(object sender, RoutedEventArgs e)
+        {
+            pestaña2Grid.DataContext = heroe;
+            MessageBox.Show("Superhéroe insertado con éxito", "Superhéroes", MessageBoxButton.OK, MessageBoxImage.Information);
+            heroes.Add(heroe);
+            ActualizaIndice(ObtenerIndiceActual());
+            LimpiarFormulario();
+        }
+
+        private void LimpiarButton(object sender, RoutedEventArgs e)
+        {
+            LimpiarFormulario();
+
+        }
+        private void LimpiarFormulario()
+        {
+            heroe = new Superheroe();
+            pestaña2Grid.DataContext = heroe;
+            heroe.Heroe = true;
+        }
+    }
+}
